7764bc12347d2643348d47ecade467efe3ff4b92
Kurgansky Andrey <andrewkurg@gmail.com>
2026-07-20 02:21:35 +0300

    Add support for TRUNCATE clause in ClickHouse

--- src/sqlfluff/dialects/dialect_clickhouse.py
+++ src/sqlfluff/dialects/dialect_clickhouse.py
@@ -1767,6 +1767,24 @@ class CreateDictionaryStatementSegment(BaseSegment):
     )
 
 
+class TruncateStatementSegment(ansi.TruncateStatementSegment):
+    """A `TRUNCATE TABLE` statement.
+
+    https://clickhouse.com/docs/sql-reference/statements/truncate#truncate-table
+    """
+
+    type = "truncate_table"
+
+    match_grammar: Matchable = Sequence(
+        "TRUNCATE",
+        "TABLE",
+        Ref("IfExistsGrammar", optional=True),
+        Ref("TableReferenceSegment"),
+        Ref("OnClusterClauseSegment", optional=True),
+        Ref.keyword("SYNC", optional=True),
+    )
+
+
 class DropTableStatementSegment(ansi.DropTableStatementSegment):
     """A `DROP TABLE` statement.
 
